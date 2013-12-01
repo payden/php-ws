ee0d80a2ce8c1d74ebf6d696f1e4b250ab9d8a0f
Payden Sutherland <payden@paydensutherland.com>
2013-12-01 18:16:39 -0500

    Add libwebsock installer and travis.yml

---
+++ install-libwebsock.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir -pv build
+cd build/
+wget http://paydensutherland.com/libwebsock-1.0.4.tar.gz
+tar -xvzf libwebsock-1.0.4.tar.gz
+cd libwebsock-1.0.4
+./configure && make && sudo make install
+cd ../..
