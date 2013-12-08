56c5f1f7efd706a1f3899b2e425c65dd6aab540d
Payden Sutherland <payden@paydensutherland.com>
2013-12-07 23:58:35 -0500

    Add ldconfig call to install-libwebsock.sh

--- install-libwebsock.sh
+++ install-libwebsock.sh
@@ -5,4 +5,5 @@ wget http://paydensutherland.com/libwebsock-1.0.4.tar.gz
 tar -xvzf libwebsock-1.0.4.tar.gz
 cd libwebsock-1.0.4
 ./configure && make && sudo make install
+sudo /sbin/ldconfig
 cd ../..
